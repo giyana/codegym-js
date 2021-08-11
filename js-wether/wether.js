857ce022033362baa29aedaa253d813b85669909
giyana <wktkktkr5@gmail.com>
2021-08-11 10:33:49 +0900

    [add]天気情報を取得し、HTMLにはんえいさせる機能実装

--- js-wether/wether.js
+++ js-wether/wether.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     //指定されたプルダウンの値を取得
     let getSelectValue = function (name) {
-        let result = '';
+        let result = "";
         let elems = document.getElementById(name).options;
         for (let i = 0, len = elems.length; i < len; i++) {
             let elem = elems.item(i);
@@ -13,51 +13,84 @@ document.addEventListener('DOMContentLoaded', function () {
         return result;
     };
 
-    //APIへの問い合わせURLを生成
+    //APIへの問い合わせ～HTML反映まで
     let getAPI = function () {
-        let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + getSelectValue('city') + '&APPID=4b5774e9f3d2a07b84f0f2f88e486224';
+        //APIへの問い合わせURLを生成
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + getSelectValue("city") + "&APPID=4b5774e9f3d2a07b84f0f2f88e486224";
+        //天気情報を取得＆HTMLに反映
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
+        xhr.open("GET", url, true);
         xhr.onload = function () {
             let data_json = this.response;
             let data = JSON.parse(data_json);
-            let weather = data.weather;
-            console.log(weather);
+            let weather_en = data.weather[0].main;
 
-            //取得した天気をHTMLに反映
+            //天候状況を日本語に翻訳（公式ドキュメントの全天候網羅済）
+            switch (weather_en) {
+                case "Clouds":
+                    var weather_jp = "曇り";
+                    break;
+                case "Clear":
+                    var weather_jp = "晴天"
+                    break;
+                case "Snow":
+                    var weather_jp = "雪"
+                    break;
+                case "Rain":
+                    var weather_jp = "雨"
+                    break;
+                case "Tornado":
+                    var weather_jp = "竜巻"
+                    break;
+                case "Squall":
+                    var weather_jp = "スコール"
+                    break;
+                case "Thunderstorm":
+                    var weather_jp = "雷雨"
+                    break;
+                case "Drizzle":
+                    var weather_jp = "霧雨"
+                    break;
+                case "Mist":
+                    var weather_jp = "霧"
+                    break;
+                case "Smoke":
+                    var weather_jp = "煙っぽい"
+                    break;
+                case "Haze":
+                    var weather_jp = "煙霧"
+                    break;
+                case "Fog":
+                    var weather_jp = "濃霧"
+                    break;
+                case "Ash":
+                    var weather_jp = "降灰"
+                    break;
+                case "Dust":
+                    var weather_jp = "砂塵"
+                    break;
+                case "Sand":
+                    var weather_jp = "砂っぽい"
+                    break;
+                default:
+                    var weather_jp = "不明"
+            }
+
+            //HTML上のプルダウンの町名取得
+            let city_en = document.getElementById("city")
+            let city_index = city_en.selectedIndex
+            let city_jp = city_en.options[city_index].text;
+
+            //取得した町名・天気をHTMLに反映
             let result_weather = document.getElementById("result");
-            let city = document.getElementById("city").value
-            //console.log(city.value);
-            result_weather.textContent = `${city}の天気は${weather}です`;
+            result_weather.textContent = `${city_jp}の天気は${weather_jp}です。`;
         };
         xhr.send();
     };
 
-    //最初のレンダリング時に問い合わせURLを生成
+    //初回レンダリング時
     getAPI();
 
-    //プルダウンを切り替えた時にAPIへの問い合わせURLを生成
-    document.getElementById('city').onchange = function () { getAPI() };
-
-
+    //プルダウンを切り替え時
+    document.getElementById("city").onchange = function () { getAPI() };
 });
-
-/*
-天気一覧
-Clouds
-Clear
-Snow
-Rain
-Drizzle
-Thunderstorm
-Mist
-Smoke
-Haze
-Dust
-Fog
-Sand
-Dust
-Ash
-Squall
-Tornado
-*/
