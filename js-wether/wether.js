a3e0fb7a750536805dd1ec8969db1b00cb8551b3
giyana <wktkktkr5@gmail.com>
2021-08-11 20:03:34 +0900

    [fix]記法をキャメルケースに統一

--- js-wether/wether.js
+++ js-wether/wether.js
@@ -19,74 +19,74 @@ document.addEventListener("DOMContentLoaded", function () {
         const cityName = getSelectValue("city");
         const appId = "4b5774e9f3d2a07b84f0f2f88e486224";
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${appId}`;
-        
+
         //天気情報を取得＆HTMLに反映
         let xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
         xhr.onload = function () {
-            let data_json = this.response;
-            let data = JSON.parse(data_json);
-            let weather_en = data.weather[0].main;
+            let dataJson = this.response;
+            let data = JSON.parse(dataJson);
+            let weatherEn = data.weather[0].main;
 
             //天候状況を日本語に翻訳（公式ドキュメントの全天候網羅済）
-            switch (weather_en) {
+            switch (weatherEn) {
                 case "Clouds":
-                    var weather_jp = "曇り";
+                    var weatherJp = "曇り";
                     break;
                 case "Clear":
-                    var weather_jp = "晴天"
+                    var weatherJp = "晴天"
                     break;
                 case "Snow":
-                    var weather_jp = "雪"
+                    var weatherJp = "雪"
                     break;
                 case "Rain":
-                    var weather_jp = "雨"
+                    var weatherJp = "雨"
                     break;
                 case "Tornado":
-                    var weather_jp = "竜巻"
+                    var weatherJp = "竜巻"
                     break;
                 case "Squall":
-                    var weather_jp = "スコール"
+                    var weatherJp = "スコール"
                     break;
                 case "Thunderstorm":
-                    var weather_jp = "雷雨"
+                    var weatherJp = "雷雨"
                     break;
                 case "Drizzle":
-                    var weather_jp = "霧雨"
+                    var weatherJp = "霧雨"
                     break;
                 case "Mist":
-                    var weather_jp = "霧"
+                    var weatherJp = "霧"
                     break;
                 case "Smoke":
-                    var weather_jp = "煙っぽい"
+                    var weatherJp = "煙っぽい"
                     break;
                 case "Haze":
-                    var weather_jp = "煙霧"
+                    var weatherJp = "煙霧"
                     break;
                 case "Fog":
-                    var weather_jp = "濃霧"
+                    var weatherJp = "濃霧"
                     break;
                 case "Ash":
-                    var weather_jp = "降灰"
+                    var weatherJp = "降灰"
                     break;
                 case "Dust":
-                    var weather_jp = "砂塵"
+                    var weatherJp = "砂塵"
                     break;
                 case "Sand":
-                    var weather_jp = "砂っぽい"
+                    var weatherJp = "砂っぽい"
                     break;
                 default:
-                    var weather_jp = "不明"
+                    var weatherJp = "不明"
             }
 
             //HTML上のプルダウンの町名取得
-            let city_en = document.getElementById("city")
-            let city_index = city_en.selectedIndex
-            let city_jp = city_en.options[city_index].text;
+            let cityEn = document.getElementById("city")
+            let cityIndex = cityEn.selectedIndex
+            let cityJp = cityEn.options[cityIndex].text;
 
             //取得した町名・天気をHTMLに反映
-            let result_weather = document.getElementById("result");
-            result_weather.textContent = `${city_jp}の天気は${weather_jp}です。`;
+            let resultWeather = document.getElementById("result");
+            resultWeather.textContent = `${cityJp}の天気は${weatherJp}です。`;
         };
         xhr.send();
     };
